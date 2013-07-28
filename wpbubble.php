4e4a0fd541a41d336073ac310f2c0af5c49a3584
Vibhav Sinha <vsinha@webdweb.com>
2013-07-28 14:40:26 +0530

    Initial Commit

---
+++ wpbubble.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ Plugin Name: WPBubble
+ Plugin URI: http://www.reportbubble.com/download.php
+ Description: Reportbubble plugin for wordpress systems.
+ Version: 1.0
+ Author: WebDweb web systems
+ Author URI: http://www.webdweb.com/
+ License: GPL2
+ */
+add_action( 'wp_footer', 'bubblemark' );
+
+function bubblemark()
+{
+	echo ':P';
+}
+?>
